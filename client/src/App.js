aec0c9b27519ed198a3859770d064a669107949c
sourabhsahu334 <18jan2002&>
2023-08-23 19:16:30 +0530

    first

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,102 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useState } from "react";
-import Chat from "./Chat";
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import './App.css';
 
-const socket = io.connect("http://localhost:3001");
+
+const socket = io('http://localhost:3001'); // Connect to the backend server
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [targetSocketID, setTargetSocketID] = useState('');
+  const [userName, setUserName] = useState('');
+  const [ isjoined ,setisjooined]=useState(false);
+  const [storedname,setstoredname]=useState("");
 
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
+  const handleJoinRoom = () => {
+    if (targetSocketID.trim() !== '' && userName.trim() !== '') {
+      setMessages([]); // Clear messages when joining a new room
+      socket.emit('join_room', targetSocketID);
+      setisjooined(true);
+      localStorage.setItem("user",userName);
+    }
+  };
+  useEffect(()=>{
+   const data=   localStorage.getItem('user');
+   setstoredname(data);
+  },[message])
+
+  const handleSendMessage = () => {
+    if (message.trim() !== '') {
+      const data = {
+        targetSocketID,
+        message,
+        name:userName
+      };
+      socket.emit('send_message', data);
+      setMessage('');
     }
   };
 
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessages(prevMessages => [...prevMessages, data]);
+    });
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    return () => {
+      socket.off('receive_message'); // Clean up event listener on component unmount
+    };
+  }, []);
+
   return (
     <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
+      <h1>Chat App</h1>
+      <h3>Joined with id or can create the new for with friends </h3>
+      <div className="join-room">
+        <input
+          type="text"
+          placeholder="Enter target socket ID..."
+          value={targetSocketID}
+          onChange={(e) => setTargetSocketID(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter your username..."
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={handleJoinRoom}>Join Room</button>
+      </div>
+     { isjoined && <div  className="message-container">
+     <div className="input-container">
           <input
             type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
+            placeholder="Enter your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
+          <button onClick={handleSendMessage}>Send</button>
         </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
+        <div  className="messages" >
+          {messages.map((msg, index) => (
+            <div key={index}  style={{display:"flex",width:"250px",alignItems:"center",marginLeft:msg.name==storedname&&"auto",overflow: "hidden" }}>
+              <p style={{color:msg.name==storedname && "rgba(0,0,0,.8)",width:"150px",wordWrap: "break-word" }}>
+             {msg.message}
+              </p>      
+              <p style={{color:"black",fontSize:"12px",paddingTop:"5px",marginInline:"5px"}}>  {new Date().toLocaleString().substring(10,15)}</p>
+            </div>
+          ))}
+        </div>
+      
+      </div>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -9,25 +9,48 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // Change to your frontend URL
     methods: ["GET", "POST"],
   },
 });
 
+// Store room participants
+const roomParticipants = {};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  socket.on("join_room", (targetSocketID) => {
+    const roomID = targetSocketID; // Use the target socket ID as the room ID
+    socket.join(roomID);
+
+    if (!roomParticipants[roomID]) {
+      roomParticipants[roomID] = [];
+    }
+    roomParticipants[roomID].push(socket.id);
+
+    console.log(`User with ID: ${socket.id} joined room: ${roomID}`);
   });
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+    const { targetSocketID, message,name } = data;
+    const senderSocketID = socket.id;
+    const roomID = targetSocketID;
+
+    io.to(roomID).emit("receive_message", {
+      senderSocketID,
+      message,
+      name
+    });
   });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
+    
+    // Remove the disconnected socket ID from all rooms
+    for (const roomID in roomParticipants) {
+      roomParticipants[roomID] = roomParticipants[roomID].filter(id => id !== socket.id);
+    }
   });
 });
 
